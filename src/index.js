509d29dc9dcd7c11c48c14a7aa09d47700243893
Nik-9649 <usmanfazaljj@gmail.com>
2022-08-10 07:50:55 +0500

    JSX Index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import profile1 from "./images/dilmcphee.jpg";
+import profile2 from "./images/shayini_venthan.jpg";
+import profile3 from "./images/vickyrjoyal.jpg";
 
 const App = () => {
   return (
     <div className="ui comments">
       <div className="comment">
         <a href="/" calssName="avatar">
-          <img alt="profile picture" />
+          <img src={profile1} alt="profile picture" width="193" height="130" />
         </a>
         <div className="content">
           <a className="author">dilmcphee</a>
