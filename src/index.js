2fb88f1c9ef3d9beb747a7ecc2ae247870bb6e65
Nik-9649 <usmanfazaljj@gmail.com>
2022-08-11 07:11:07 +0500

    .

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import profile1 from "./images/dilmcphee.jpg";
+import profile2 from "./images/shayini_venthan.jpg";
+import profile3 from "./images/vickyrjoyal.jpg";
 import SingleComment from "./SingleComment";
 
 const App = () => {
   return (
     <div className="ui comments">
-      <SingleComment name="dilmcphee" comment="Blossom" />
-      <SingleComment name="shayini_venthan" comment="Hazel" />
-      <SingleComment name="vickyrjoyal" comment="Alfred 👑" />
+      <SingleComment
+        profilePicture={profile1}
+        name="dilmcphee"
+        comment="Blossom"
+      />
+      <SingleComment
+        profilePicture={profile2}
+        name="shayini_venthan"
+        comment="Hazel"
+      />
+      <SingleComment
+        profilePicture={profile3}
+        name="vickyrjoyal"
+        comment="Alfred 👑"
+      />
     </div>
   );
 };
