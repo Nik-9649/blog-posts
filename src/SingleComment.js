73d3be4ff84e99b60633da9c4a1e321691b659b2
Nik-9649 <usmanfazaljj@gmail.com>
2022-08-11 05:28:57 +0500

    .

---
+++ src/SingleComment.js
@@ -0,0 +1,25 @@
+import React from "react";
+import profile1 from "./images/dilmcphee.jpg";
+import profile2 from "./images/shayini_venthan.jpg";
+import profile3 from "./images/vickyrjoyal.jpg";
+
+const SingleComment = () => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img src={profile1} alt="profile picture" />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          dilmcphee
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 5:00PM</span>
+        </div>
+        <div className="text">Blossom</div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleComment;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import profile1 from "./images/dilmcphee.jpg";
-import profile2 from "./images/shayini_venthan.jpg";
-import profile3 from "./images/vickyrjoyal.jpg";
+import SingleComment from "./SingleComment";
 
 const App = () => {
   return (
     <div className="ui comments">
-      <div className="comment">
-        <a href="/" calssName="avatar">
-          <img src={profile1} alt="profile picture" width="193" height="130" />
-        </a>
-        <div className="content">
-          <a className="author">dilmcphee</a>
-          <div className="metadata">
-            <span className="date">Today at 5:00PM</span>
-            <div className="text">Blossom</div>
-          </div>
-        </div>
-      </div>
+      <SingleComment />
+      <SingleComment />
+      <SingleComment />
     </div>
   );
 };
